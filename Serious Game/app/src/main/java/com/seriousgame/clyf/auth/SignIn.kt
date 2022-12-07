f5a754bc9609d59484701e5ff8181fed4af0e2a8
FiliBell <117976345+FiliBell@users.noreply.github.com>
2022-12-07 18:06:00 +0100

    setting CRUD (admin)

--- Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignIn.kt
+++ Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignIn.kt
@@ -1,23 +1,28 @@
 package com.seriousgame.clyf.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.seriousgame.clyf.MainActivity
-import com.seriousgame.clyf.R
+import com.seriousgame.clyf.admin.AdminActivity
 import com.seriousgame.clyf.databinding.ActivitySignInBinding
-import com.seriousgame.clyf.databinding.ActivitySignUpBinding
+import java.util.Objects
+
 
 class SignIn : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding //feature that allows you to more easily write code that interacts with views (replaces findViewById) - F.
     private lateinit var firebaseAuth: FirebaseAuth //define FirebaseAuth - F.
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater) //binding - S.
         setContentView(binding.root)    //get XML ActivitySignIn root with binding - F.
 
         firebaseAuth = FirebaseAuth.getInstance()
@@ -27,24 +32,31 @@ class SignIn : AppCompatActivity() {
             val email = binding.emailET.text.toString() //email field - F.
             val pass = binding.passET.text.toString()   //pass field - F.
 
+            //start if - S.
             if (email.isNotEmpty() && pass.isNotEmpty()){   //check form field - F.
                     firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {    //check user data in Firebase - F.
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
+
+                        //1st inner if - S.
+                        if (it.isSuccessful) { //successful case - S.
+                            appoggioID = email
+                            val intent = Intent(this, AdminActivity::class.java) //define intent - S.
+                            startActivity(intent) //takes you to SignIn activity - S.
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show() //error message - F.
                         }
+                        //end 1st inner if - S.
                     }
             } else{
                 Toast.makeText(this, "Errore: completa tutti i campi", Toast.LENGTH_LONG).show()    //error message - F.
             }
-
+            //end if - S.
         }
 
         binding.signinSignup.setOnClickListener {   //if you are not already registered it takes us to SignUp activity - F.
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+
     }
 }
--- Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignUp.kt
+++ Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignUp.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.seriousgame.clyf.R
 import com.seriousgame.clyf.databinding.ActivityMainBinding
 import com.seriousgame.clyf.databinding.ActivitySignUpBinding
@@ -17,34 +18,55 @@ class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater) //binding - S.
         setContentView(binding.root)    //get XML ActivitySignUp root with binding - F.
 
         firebaseAuth = FirebaseAuth.getInstance()
         //assign to firebaseAuth a method that returns an instance of this class corresponding to the default FirebaseApp instance - F.
+        //db = FirebaseFirestore.getInstance()    // Access a Cloud Firestore instance from your Activity -F.
 
         binding.signupButton.setOnClickListener {   //at signupButton click - F.
             val email = binding.emailET.text.toString() //email field - F.
             val pass = binding.passET.text.toString()   //pass field - F.
             val confirmpass = binding.confirmPassEt.text.toString() //confirm_pass field - F.
 
+            //start if - S.
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmpass.isNotEmpty()){   //check form field - F.
+                //1st inner if - S.
                 if (pass == confirmpass){
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {    //add new user in Firebase with email and pass - F.
-                        //password must have almost six char!!
-                        if (it.isSuccessful){
-                            val intent = Intent(this, SignIn::class.java)
+                        //FirebaseAuth: password must have almost six char - S.
+
+                        //2nd inner if - S.
+                        if (it.isSuccessful){ //succesful case - S.
+
+                            //insert the user information into the MutableMap - F.
+                            var informazioni : MutableMap<String, Any> = hashMapOf()
+                            informazioni["Email"] = email
+                            informazioni["Pass"] = pass
+
+                            //insert the MutableMap (information) into the MutableMap (dati) - F.
+                            var dati : MutableMap<String, Any>  = hashMapOf()
+                            dati["Dati"] = informazioni
+
+                            //add a new document with email as ID and insert into it the MultableMap named dati - F.
+                            db.collection("Users").document(email).set(dati)
+
+                            val intent = Intent(this, SignIn::class.java) //define intent - S.
                             startActivity(intent)   //takes you to SignIn activity - F.
                         }else{
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show() //error message - F.
                         }
+                        //end 2nd inner if - S.
                     }
                 }else{
                     Toast.makeText(this, "Le password non sono coincidenti", Toast.LENGTH_LONG).show()  //error message - F.
                 }
+                //end 1st inner if - S.
             }else{
                 Toast.makeText(this, "Errore: completa tutti i campi", Toast.LENGTH_LONG).show()    //error message - F.
             }
+            //end if - S.
         }
 
         binding.signupSignin.setOnClickListener {   //if you have already registered it takes us to SignIn activity - F.
