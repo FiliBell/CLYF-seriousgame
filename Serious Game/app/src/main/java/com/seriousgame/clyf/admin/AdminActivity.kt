e2010b01d313515d959c6c1101f2f1d86fed4887
FiliBell <117976345+FiliBell@users.noreply.github.com>
2022-12-12 15:07:02 +0100

    storeging

--- Serious Game/app/src/main/java/com/seriousgame/clyf/admin/AdminActivity.kt
+++ Serious Game/app/src/main/java/com/seriousgame/clyf/admin/AdminActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.Source
 import com.seriousgame.clyf.R
 import com.seriousgame.clyf.auth.appoggioID
 import com.seriousgame.clyf.auth.db
@@ -17,11 +22,16 @@ import kotlinx.android.synthetic.main.popup_create.*
 import kotlinx.android.synthetic.main.popup_create.view.*
 import kotlinx.android.synthetic.main.popup_domande.view.*
 import kotlinx.android.synthetic.main.popup_modify.*
+import kotlinx.android.synthetic.main.popup_modify.view.*
 
 class AdminActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        var questionList : ArrayList<String> = ArrayList()
+        var test : String? = null
 
         create.setOnClickListener {
             var dialogBuilder : AlertDialog.Builder
@@ -100,13 +110,98 @@ class AdminActivity : AppCompatActivity() {
         }
 
         modify.setOnClickListener {
+
             var dialogBuilder3 : AlertDialog.Builder
             var dialog3 : AlertDialog?
             var view3 = LayoutInflater.from(this).inflate(R.layout.popup_modify, null, false)
+            var modifyButton = view3.modify1
 
             dialogBuilder3 = AlertDialog.Builder(this).setView(view3)
             dialog3 = dialogBuilder3!!.create()
             dialog3.show()
+
+            db.collection(appoggioID).whereNotEqualTo("Domanda", null)
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        questionList.add(document.data["Domanda"].toString())
+                    }
+
+                    var spinner : Spinner = view3.spinner
+                    var adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, questionList)
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    spinner.adapter = adapter
+
+                    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                            test = questionList[position]
+                            Toast.makeText(applicationContext, "The selected question is " + questionList[position], Toast.LENGTH_LONG).show()
+                        }
+                        override fun onNothingSelected(p0: AdapterView<*>?) {
+                            Toast.makeText(applicationContext, "Nothing selected", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext, "You need to insert questions first!", Toast.LENGTH_LONG).show()
+                    dialog3.dismiss()
+                }
+
+            modifyButton.setOnClickListener {
+                if (test != null){
+                    var dialogBuilder4 : AlertDialog.Builder
+                    var dialog4 : AlertDialog?
+                    var view4 = LayoutInflater.from(applicationContext).inflate(R.layout.popup_domande, null, false)
+                    var question = view4.question
+                    var answer1 = view4.answer1
+                    var answer2 = view4.answer2
+                    var answer3 = view4.answer3
+                    var correctAnswer = view4.correctanswer
+                    var save = view4.save2
+                    var quiz : MutableMap<String, Any>
+
+                    dialogBuilder4 = AlertDialog.Builder(this).setView(view4)
+                    dialog4 = dialogBuilder4!!.create()
+                    dialog4.show()
+
+                    save.setOnClickListener {
+                        if (!TextUtils.isEmpty(question.text) && !TextUtils.isEmpty(answer1.text) && !TextUtils.isEmpty(answer2.text) && !TextUtils.isEmpty(answer3.text) && !TextUtils.isEmpty(correctAnswer.text)){
+                            if ((correctAnswer.text.toString() == answer1.text.toString()) || ((correctAnswer.text.toString() == answer2.text.toString()) || (correctAnswer.text.toString() == answer3.text.toString()))){
+                                quiz = hashMapOf()
+                                quiz["Domanda"] = question.text.toString()
+                                quiz["Risposta1"] = answer1.text.toString()
+                                quiz["Risposta2"] = answer2.text.toString()
+                                quiz["Risposta3"] = answer3.text.toString()
+                                quiz["Risposta_Corretta"] = correctAnswer.text.toString()
+
+                                /*
+                                db.collection(appoggioID).whereEqualTo("Domanda", test)
+                                    .get()
+                                    .addOnSuccessListener { documents ->
+                                        for (document in documents){
+                                            db.collection(appoggioID).document().set(quiz, SetOptions.merge())
+                                        }
+                                    }
+
+
+                                 */
+                                dialog4.dismiss()
+                                dialog3.dismiss()
+                            }else{
+                                Toast.makeText(this, "La risposta corretta deve essere uguale a una delle risposte", Toast.LENGTH_LONG).show()
+                            }
+                        }else{
+                            Toast.makeText(this, "Non hai inserito un campo", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+
+                }else{
+                    Toast.makeText(this, "You need to select one question first!", Toast.LENGTH_LONG).show()
+                }
+            }
+
 
         }
 
